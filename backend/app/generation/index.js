9e9906a6ecc27f98d226a642554fc8a0654ea43e
Ethan Hurst <Ethan.Hurst@jjrichards.com.au>
2019-08-27 11:44:56 +1000

    more file dir changes

--- backend/app/generation/index.js
+++ backend/app/generation/index.js
@@ -1,6 +1,6 @@
-const { REFRESH_RATE, SECONDS } = require('./config')
+const { REFRESH_RATE, SECONDS } = require('../config')
 const refreshRate = REFRESH_RATE * SECONDS
-const Dragon = require('./dragon')
+const Dragon = require('../dragon')
 
 class Generation {
   constructor() {
--- backend/default.js
+++ backend/default.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const GenerationEngine = require('./app/engine')
+const GenerationEngine = require('./app/generation/engine.js')
 const app = express();
 const engine = new GenerationEngine()
 
